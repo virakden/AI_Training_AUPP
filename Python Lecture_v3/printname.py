2feefb2424aeb6e09435c80d9d52449ee1bc82e8
virakden <virakden1717@gmail.com>
2024-12-13 18:15:50 +0800

    upload source code from training at aupp

---
+++ Python Lecture_v3/printname.py
@@ -0,0 +1,13 @@
+import sys
+
+
+#if len(sys.argv) < 2:
+#    print("Too few arguments")
+#elif len(sys.argv) > 2:
+#    print("Too many arguments")
+#else:
+#    print("hello, my name is", sys.argv[1])
+
+#'''
+
+print(sys.argv[1:])
---
+++ Python Lecture_v3/slicetest.py
@@ -0,0 +1,7 @@
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
+
+for arg in sys.argv:
+    print("hello, my name is", arg)
